c027c117ba04eadf64cb28650e95ec72cdbe16e6
monetree <soubhagyakumar666@gmail.com>
2018-11-07 15:04:25 +0530

    adding create-react-sass-typescript app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import './App.css';
+
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="welcome">
+        <h1>Hello World !</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
